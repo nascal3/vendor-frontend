0eb3207cf6a770dd6b63f0a68513194843e3843d
nascal3 <nascal3@gmail.com>
2019-02-20 14:10:10 +0300

    added web icon and name on top the sidebar and background color

--- src/components/SideNavbar/SideNavbar.vue
+++ src/components/SideNavbar/SideNavbar.vue
@@ -1,7 +1,42 @@
 <template>
   <nav>
-    <v-navigation-drawer app v-model="drawer" class="primary">
-      <v-list>
+    <v-navigation-drawer app v-model="drawer" :mini-variant="mini" dark>
+      <v-list class="pa-1">
+        <v-list-tile v-if="mini" @click.stop="mini = !mini">
+          <v-list-tile-action>
+            <v-icon class="white--text">chevron_right</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+
+        <v-list-tile avatar tag="div">
+
+          <router-link to="/home">
+            <div class="barIcon__circle">
+              <v-list-tile-avatar>
+                <svg class="barIcon__circle-icon">
+                  <use xlink:href="@/assets/icons/icons-symbols.svg#icon-truck"></use>
+                </svg>
+              </v-list-tile-avatar>
+            </div>
+          </router-link>
+
+          <v-list-tile-content>
+            <v-list-tile-title class="sidebarLink">
+              <router-link class="subheading text-uppercase font-weight-light white--text" to="/home">
+                Vendor Portal
+              </router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+
+          <v-list-tile-action>
+            <v-btn icon @click.stop="mini = !mini">
+              <v-icon class="white--text">chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+      <v-list dense>
+        <v-divider class="grey darken-2"></v-divider>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-tile-action>
             <v-icon class="white--text">{{link.icon}}</v-icon>
@@ -27,8 +62,10 @@ export default {
   data () {
     return {
       drawer: false,
+      mini: false,
+      right: null,
       links: [
-        { icon: 'dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'dashboard', text: 'Dashboard', route: '/home' },
         { icon: 'folder', text: 'My Projects', route: '/projects' },
         { icon: 'person', text: 'team', route: '/team' }
       ]
