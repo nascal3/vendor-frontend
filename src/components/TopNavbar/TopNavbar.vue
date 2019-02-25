7147d6e8071db5139cd0371a1b865fb4b2f905f9
nascal3 <nascal3@gmail.com>
2019-02-25 12:05:57 +0300

    hide sidebar and avator if used not logged in

--- src/components/TopNavbar/TopNavbar.vue
+++ src/components/TopNavbar/TopNavbar.vue
@@ -1,19 +1,20 @@
 <template>
   <nav>
     <v-toolbar app light class="text--darken-4">
-      <v-toolbar-side-icon class="blue-grey--text text--darken-4" @click="toggleSidebar"></v-toolbar-side-icon>
+      <v-toolbar-side-icon v-if="loggedIn" class="blue-grey--text text--darken-4" @click="toggleSidebar"></v-toolbar-side-icon>
       <v-spacer></v-spacer>
       <div class="logo">
         <v-img class="logo__topNav" :src="LogoImage" alt="tuskys logo"></v-img>
       </div>
       <v-spacer></v-spacer>
-      <AvatorSection></AvatorSection>
+      <AvatorSection v-if="loggedIn"></AvatorSection>
     </v-toolbar>
   </nav>
 </template>
 
 <script>
 import AvatorSection from '@/components/AvatorSection/AvatorSection'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'TopNavbar',
@@ -31,6 +32,11 @@ export default {
       this.open = !this.open
       this.$emit('toggleBar', this.open)
     }
+  },
+  computed: {
+    ...mapGetters({
+      loggedIn: 'loggedIn'
+    })
   }
 }
 </script>
