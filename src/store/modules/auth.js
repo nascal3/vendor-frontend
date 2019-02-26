b15dc138d6b4153774a994001a4bc8baa2813fb9
nascal3 <nascal3@gmail.com>
2019-02-26 18:02:59 +0300

    added login logic animations

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,6 +1,7 @@
 import axios from '@/components/Interceptors/axios-interceptor'
 
 const state = {
+  loading: false,
   loggedIn: false,
   tokenID: null,
   loginError: null,
@@ -9,10 +10,16 @@ const state = {
 
 const mutations = {
   AUTH_USER (state, userData) {
-    state.tokenID = userData
+    state.tokenID = localStorage.setItem('token', userData.token)
+  },
+  USER_LOGGEDIN (state, data) {
+    state.loggedIn = data
   },
   LOGIN_ERROR (state, data) {
     state.loginError = data
+  },
+  SHOW_LOADER (state, data) {
+    state.loading = data
   },
   GET_FAKE_DATA (state, results) {
     state.fakeData = results.data
@@ -20,12 +27,15 @@ const mutations = {
 }
 
 const actions = {
-  loginUser ({ commit }, userData) {
-    console.log('run', userData)
+  loginUser ({ commit, state }, userData) {
+    commit('SHOW_LOADER', true)
     axios.post('/users/login', userData).then((res) => {
-      console.log('>>X', res)
-      commit('AUTH_USER', res)
+      commit('AUTH_USER', res.data)
+
+      commit('USER_LOGGEDIN', true)
+      commit('SHOW_LOADER', false)
     }).catch(err => {
+      commit('SHOW_LOADER', false)
       commit('LOGIN_ERROR', true)
       console.error('Error occurred!: ', err)
     })
@@ -49,8 +59,10 @@ const getters = {
   },
   loginError: state => {
     return state.loginError
+  },
+  showLoader: state => {
+    return state.loading
   }
-
 }
 
 export default {
