d7b3979ed1c870eaa7afbc49911ccb556af2b273
nascal3 <nascal3@gmail.com>
2019-02-27 12:21:18 +0300

    removed unnecessary code

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -26,26 +26,7 @@ import { mapGetters, mapActions } from 'vuex'
 export default {
   data () {
     return {
-      labels: [
-        '12am',
-        '3am',
-        '6am',
-        '9am',
-        '12pm',
-        '3pm',
-        '6pm',
-        '9pm'
-      ],
-      value: [
-        200,
-        675,
-        410,
-        390,
-        310,
-        460,
-        250,
-        240
-      ]
+
     }
   },
   computed: {
