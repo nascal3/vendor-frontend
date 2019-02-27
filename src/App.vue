6a9531f3a82aeb45ff0ec689df87a4551a69fde9
nascal3 <nascal3@gmail.com>
2019-02-27 12:20:46 +0300

    Added display of the users full name at top of page

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,18 @@
         leave-active-class="bounceOutRight"
         mode="out-in"
       >
+        <section class="userTitle" v-if="loggedIn && userFullname">
+          <span>Welcome</span>&nbsp;
+          <span class="text-lowercase">{{userFullname}}</span>
+        </section>
+      </transition>
+
+      <transition
+        name="bounce"
+        enter-active-class="bounceInLeft"
+        leave-active-class="bounceOutRight"
+        mode="out-in"
+      >
         <router-view></router-view>
       </transition>
     </v-content>
@@ -19,12 +31,15 @@
 
 import TopNavBar from '@/components/TopNavbar/TopNavbar'
 import SideNavBar from '@/components/SideNavbar/SideNavbar'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'App',
   data () {
     return {
-      navSate: false
+      navSate: false,
+      userInfo: {},
+      userFullname: ''
     }
   },
   components: {
@@ -35,6 +50,29 @@ export default {
     changeBarState (value) {
       this.navSate = value
     }
+  },
+  computed: {
+    ...mapGetters({
+      userData: 'userData',
+      loggedIn: 'loggedIn'
+    })
+  },
+  mounted () {
+    this.userInfo = JSON.parse(this.userData)
+    this.userFullname = this.userInfo.Fullname
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  @import "./styles/main";
+  .userTitle {
+    color: $primary;
+    margin: 3rem 2rem .5rem 2rem;
+    font-size: 2.5rem;
+    font-weight: 300;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  }
+</style>
