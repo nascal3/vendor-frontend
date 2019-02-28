58fc82ce8e13fc24d0f94dceffd38448cf3e4e60
nascal3 <nascal3@gmail.com>
2019-02-28 11:59:01 +0300

    added sales store module

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,14 @@ import Vuex from 'vuex'
 
 import auth from './modules/auth'
 import * as action from './actions'
+import sales from './modules/sales'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   action,
   modules: {
-    auth
+    auth,
+    sales
   }
 })
