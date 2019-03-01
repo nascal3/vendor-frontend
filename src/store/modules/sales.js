e5c55b7cdea3d14438ded04214fe04a1e35b323e
nascal3 <nascal3@gmail.com>
2019-03-01 14:52:41 +0300

    added loading animation state into store

--- src/store/modules/sales.js
+++ src/store/modules/sales.js
@@ -1,28 +1,40 @@
 import axios from '@/components/Interceptors/axios-interceptor'
 
 const state = {
-  salesTransactions: localStorage.getItem('salesTrans')
+  salesTransactions: localStorage.getItem('salesTrans'),
+  loading: false
 }
 
 const mutations = {
   FETCH_TRANSACTIONS (state, results) {
     localStorage.setItem('salesTrans', JSON.stringify(results.data))
     state.salesTransactions = localStorage.getItem('salesTrans')
+  },
+  SHOW_LOADER (state, data) {
+    state.loading = data
   }
 
 }
 
 const actions = {
   fetchTransactions ({ commit }, dataValues) {
-    axios.get('items/item/186076/1').then((res) => {
+    commit('SHOW_LOADER', true)
+    axios.get(`items/item/${dataValues.Vendor_No}/${dataValues.page}`).then((res) => {
       commit('FETCH_TRANSACTIONS', res)
-    }).catch(err => console.error('Error occurred!: ', err.message))
+      commit('SHOW_LOADER', false)
+    }).catch(err => {
+      commit('SHOW_LOADER', false)
+      console.error('Error occurred!: ', err.message)
+    })
   }
 }
 
 const getters = {
   salesTrans: state => {
     return state.salesTransactions
+  },
+  loading: state => {
+    return state.loading
   }
 }
 
