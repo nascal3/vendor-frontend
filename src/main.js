c9f750d67754607b5afa3454282c6eab7280ca68
nascal3 <nascal3@gmail.com>
2019-03-02 12:31:00 +0300

    added filters for currency and date

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import Vue2Filters from 'vue2-filters'
+Vue.use(require('vue-moment'))
 
+Vue.use(Vue2Filters)
 Vue.config.productionTip = false
 
 new Vue({
